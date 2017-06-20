1fa8427746a14b006102f1dbf70b6240523b40bb
Franz Neubert <neubert@ubilabs.net>
2017-06-20 15:33:59 +0200

    feat(webpack): don't match jsx files

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
